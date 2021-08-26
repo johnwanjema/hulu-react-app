3126b00acf09bcf70a3dfd77f1e27c962cc02439
John Wanjema <jonwanjema@gmail.com>
2021-08-26 23:16:55 +0300

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode:'jit',
+    purge: [ './pages/**/*.{js,ts,jsk,tsk}','./components/**/*.{js,ts,jsk,tsk}',],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
